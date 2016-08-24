97305b93e18f8933a37d2096c19740357d2fc2dc
Peter Lee <peter@bizflow.com>
2016-08-24 18:51:55 -0400

    Change message to display which license pattern is picked up

--- index.js
+++ index.js
@@ -42,9 +42,10 @@ for(var i = 0, len1 = sourceDir.length; i < len1; i++) {
         var patternFilePath = path + '/' + licensePattern;
         try {
           fs.accessSync(patternFilePath);
-          console.log("Source [" + patternFilePath + " ==> Target [" + fileName + "]");
           fs.createReadStream(patternFilePath)
               .pipe(fs.createWriteStream(fileName));
+
+          console.log(files[y] + " ==> " + licensePattern);
           return false;
         }
         catch(e) {}
